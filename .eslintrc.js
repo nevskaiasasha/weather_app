8a69170b9ffec9921f5e913316362ff8c888d59b
nevskaiasasha <sashafomina3@gmail.com>
2022-11-16 14:36:34 +0200

    webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      "jest/globals": true,
-    },
-    extends: ["airbnb-base", "prettier"],
-    overrides: [],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    plugins: ["jest"],
-    rules: {
-      "import/prefer-default-export": "off",
-      "no-alert": "off",
-      "max-len": [
-        "error",
-        {
-          ignoreComments: true,
-        },
-      ],
-    },
-  };
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-alert': 'off',
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+      },
+    ],
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '../css/styles.css';
+
+const h1E1 = document.body.querySelector('h1');
+
+h1E1.textContent = 'Webpack!!!';
